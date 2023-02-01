4423e9296428fe00d653e22081e133a0f2ec8423
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-02-01 18:07:50 +0300

    format

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,14 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({
-  log: ["query"],
-  errorFormat: "minimal",
+  log: ['query'],
+  errorFormat: 'minimal',
 });
 
 const connectDB = () => {
   try {
     prisma.$connect();
-    console.log("Database connection !");
+    console.log('Database connection !');
   } catch (error) {
     console.log(error);
     process.exit(1);
