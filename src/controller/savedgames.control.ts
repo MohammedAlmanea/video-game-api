9a3cd4f402d4c252f0ade689aecf0172a51ec849
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-02-01 18:15:00 +0300

    Created savegames controller

---
+++ src/controller/savedgames.control.ts
@@ -0,0 +1,54 @@
+import { Game, UserSavedGames } from '@prisma/client';
+import { Request, Response } from 'express';
+import { prisma } from '../config/db';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+
+export const saveGame = async (req: Request, res: Response) => {
+  try {
+    const game = req.body as UserSavedGames;
+    await prisma.userSavedGames.create({
+      data: game,
+    });
+    res.status(200).json({
+      message: 'Game saved',
+    });
+  } catch (error) {
+    const prismaError = error as PrismaClientKnownRequestError;
+    return res.status(400).json({
+      message: prismaError.message,
+    });
+  }
+};
+
+export const deleteSave = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    await prisma.userSavedGames.delete({
+      where: { id: id },
+    });
+    res.status(200).json({
+      message: 'Save deleted',
+    });
+  } catch (error) {
+    const prismaError = error as PrismaClientKnownRequestError;
+    return res.status(400).json({
+      message: prismaError.message,
+    });
+  }
+};
+
+export const getSavedGames = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const games = await prisma.userSavedGames.findMany({
+      where: { user_id: id },
+    });
+    res.status(200).json(games);
+  } catch (error) {
+    const prismaError = error as PrismaClientKnownRequestError;
+    return res.status(400).json({
+      message: prismaError.message,
+    });
+  }
+};
