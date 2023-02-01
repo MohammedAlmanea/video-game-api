3cdc0635445b0e474652c16a3b76976dbafe83e9
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-02-01 18:16:01 +0300

    Created savegames router

---
+++ src/routers/savedgames.router.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import {
+  saveGame,
+  deleteSave,
+  getSavedGames,
+} from '../controller/savedgames.control';
+import Validate from '../middleware/validate';
+import { savedGamesSchema } from '../zodSchema/savedgames.schema';
+
+const router = express.Router();
+
+router.post('/games/save', Validate(savedGamesSchema), saveGame);
+router.get('/games/save/:id', Validate(savedGamesSchema), getSavedGames);
+router.delete('/games/save/:id', Validate(savedGamesSchema), deleteSave);
+
+export default router;
