2cdcd98a210334f53a8522286843eba4b7de8a02
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-02-02 11:16:24 +0300

    Added new methods route

--- src/routers/game.router.ts
+++ src/routers/game.router.ts
@@ -10,6 +10,8 @@ import {
   updateGame,
   gamesByName,
   gamesByRating,
+  gamesByPriceAscend,
+  gamesByPriceDesc,
 } from '../controller/game.control';
 import Validate from '../middleware/validate';
 import { gameSchema } from '../zodSchema/game.schema';
@@ -22,6 +24,8 @@ router.get('/games/name/:name', gamesByName);
 router.get('/games/sort/asc', gamesByStoryPlayTimeAscend);
 router.get('/games/sort/des', gamesByStoryPlayTimeDesc);
 router.get('/games/sort/sales', gamesBySales);
+router.get('/games/sort/price/asc', gamesByPriceAscend);
+router.get('/games/sort/price/des', gamesByPriceDesc);
 router.get('/games/sort/rating', gamesByRating);
 router.post('/games', Validate(gameSchema), addGame);
 router.delete('/games/:id', deleteGame);
