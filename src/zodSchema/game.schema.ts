1a38e0b061ad6e6b3df18cc8b4aa513494e0723b
MohammedAlmanea <MohammedAlmanea@outlook.com>
2023-02-02 17:39:38 +0300

    Removed sales And added price also changed description max char

--- src/zodSchema/game.schema.ts
+++ src/zodSchema/game.schema.ts
@@ -20,19 +20,13 @@ export const gameSchema = z.object({
         invalid_type_error: 'description must be a string',
       })
       .min(10, 'description must be at least 10 characters long')
-      .max(50, 'description must be at most 50 characters long'),
+      .max(200, 'description must be at most 200 characters long'),
     story_playtime: z
       .number({
         required_error: 'story_playtime is required',
         invalid_type_error: 'story_playtime must be a number',
       })
       .min(1, 'story_playtime must be greater than or equal to 1'),
-    sales: z
-      .number({
-        required_error: 'sales is required',
-        invalid_type_error: 'sales must be a number',
-      })
-      .min(1, 'sales must be greater than or equal to 1'),
     rating: z
       .number({
         required_error: 'rating is required',
@@ -40,8 +34,11 @@ export const gameSchema = z.object({
       })
       .min(0, 'rating must be greater than or equal to 0')
       .max(100, 'rating must be at most 100'),
+    price: z.number({
+      required_error: 'price is require',
+      invalid_type_error: 'price must be a number',
+    }),
   }),
 });
-
 
 export type GametypeSchema = TypeOf<typeof gameSchema>['body'];
